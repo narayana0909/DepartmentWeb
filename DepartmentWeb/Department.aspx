a68d1a29e136f0799db07a6d3dee8695a0fe00d5
Narayana Swami Potla <narayanaswamipotla2002@gmail.com>
2025-09-16 11:22:37 +0530

    Add project files.

---
+++ DepartmentWeb/Department.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Department.aspx.cs" Inherits="DepartmentWeb.Department" %>
+
+<!DOCTYPE html>
+<head runat="server">
+    <title>Department Management</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Add Department</h2>
+            <asp:TextBox ID="txtDeptName" runat="server" Placeholder="Department Name"></asp:TextBox><br /><br />
+            <asp:TextBox ID="txtDeptID" runat="server" Placeholder="Department ID"></asp:TextBox><br /><br />
+
+            <asp:TextBox ID="txtLocation" runat="server" Placeholder="Location"></asp:TextBox><br /><br />
+            <asp:Button ID="btnAdd" runat="server" Text="Add Department" OnClick="btnAdd_Click" /><br /><br />
+            <asp:Label ID="lblMessage" runat="server" ForeColor="Green"></asp:Label>
+
+            <h2>All Departments</h2>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true" Visible="false"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DepartmentWeb/Department.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace DepartmentWeb
+{
+    public partial class Department : System.Web.UI.Page
+    {
+        String cs = ConfigurationManager.ConnectionStrings["Company"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindDepartments();
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection con = new MySqlConnection(cs))
+            {
+                string query = "INSERT INTO Departments (DepartmentName, DepartmentID,Location) VALUES (@name, @ID, @location)";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@name", txtDeptName.Text);
+                cmd.Parameters.AddWithValue("@ID", txtDeptID.Text);
+                cmd.Parameters.AddWithValue("@location", txtLocation.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                lblMessage.Text = "Department Added Successfully!";
+
+            }
+
+
+        }
+
+        private void BindDepartments()
+        {
+            using (MySqlConnection con = new MySqlConnection(cs))
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM Departments", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
